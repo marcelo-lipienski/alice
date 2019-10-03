58b96ba465bb1189a6d0f1b096260f8bf7b587e5
Marcelo Lipienski <marcelo.lipienski@gmail.com>
2019-10-03 06:28:58 -0300

    Updates Property controller to consume the Service Provider

--- src/controllers/Property.js
+++ src/controllers/Property.js
@@ -1,15 +1,75 @@
 import express from 'express'
-import PropertyService from '../services/Property.js'
+import passport from 'passport'
+import Property from '../models/Property'
+import ServiceProvider from '../services/ServiceProvider'
 
 const router = express.Router()
 
-router.get('/', async function (req, res) {
-  const properties = await PropertyService.all()
-  res.json(properties)
-})
+let Provider = new ServiceProvider(Property)
 
-router.post('/', async function (req, res) {
+/**
+ * GET /properties
+ */
+router.get('/', passport.authenticate('jwt', { session: false }),
+  async function (req, res, next) {
+    const response = await Provider.all()
+    res.status(response.status).json(response)
+  }
+)
 
-})
+/**
+ * GET /properties/:id
+ */
+router.get('/:id', passport.authenticate('jwt', { session: false }),
+  async function (req, res) {
+    const { id } = req.params
+    const response = await Provider.get(id)
+    res.status(response.status).json(response)
+  }
+)
+
+/**
+ * POST /properties/
+ * @param {string} ref Property reference code
+ * @param {number} price Property rent
+ */
+router.post('/', passport.authenticate('jwt', { session: false }),
+  async function (req, res) {
+    const { ref, price }  = req.body
+    const response = await Provider.create({ ref, price }, 'ref')
+    res.status(response.status).json(response)
+  }
+)
+
+/**
+ * PUT /properties/:id
+ * @param {string} id A valid mongoose ObjectId
+ * @param {number} price Property rent
+ * @param {number} area Property area
+ */
+router.put('/:id', passport.authenticate('jwt', { session: false }),
+  async function (req, res) {
+    const id = req.params.id
+    const params = {
+      price: req.body.price,
+      area: req.body.area
+    }
+
+    const response = await Provider.update(id, params)
+    res.status(response.status).json(response)
+  }
+)
+
+/**
+ * DELETE /properties/:id
+ * @param {string} id A valid mongoose ObjectId
+ */
+router.delete('/:id', passport.authenticate('jwt', { session: false }),
+  async function (req, res) {
+    const id = req.params.id
+    const response = await Provider.delete(id)
+    res.status(response.status).json(response)
+  }
+)
 
 export default router
--- src/models/Property.js
+++ src/models/Property.js
@@ -2,7 +2,9 @@ import mongoose, { Schema } from 'mongoose'
 
 const propertySchema = new Schema({
   _id: Schema.Types.ObjectId,
-  ref: String
+  ref: { type: String, required: true },
+  price: Number,
+  area: Number
 })
 
 module.exports = mongoose.model('Property', propertySchema)
